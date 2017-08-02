748b11afdf11580f8d2e8822e75373d04d2b051d
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2017-08-02 22:43:35 +0100

    Add JS data types
    Add string, number and boolean data types

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var personName = 'Kwasi Adutwum';
+console.log(personName);
+
+var lastName = 'Asiedu';
+console.log(lastName);
+
+var age = 35;
+console.log(age);
+
+var completeAge = true;
+console.log(completeAge);
