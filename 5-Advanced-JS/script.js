20adad49a50e3672f9aab9d718267c3ecc9eed18
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-01-27 20:33:47 +0100

    Add function 'arrayCalc'

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -108,3 +108,11 @@ console.log(obj.city);
 // Lecture: Passing functions as arguments
 
 var years = [1823, 1963, 1743, 2006, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0, i < arr.length, i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
