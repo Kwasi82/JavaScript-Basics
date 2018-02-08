50b442afe67d959a03002b36b7509b8c6d238f51
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-08 20:32:46 +0100

    Add space in else if statement console.log.

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -259,7 +259,7 @@ var kwasi = {
         if (style === 'formal') {
             console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old.');
         } else if (style === 'friendly') {
-            console.log('Hey! What\'s up I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+            console.log('Hey! What\'s up I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
         }
     }
 };
