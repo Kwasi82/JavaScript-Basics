96dd38b1039aeec5ef3357a375b1763337ba50d8
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-08 20:29:37 +0100

    Add 'presentation' method to 'kwasi' object

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -253,4 +253,11 @@ var kwasi = {
     name: 'Kwasi',
     age: 35,
     job: 'Web Developer',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
 };
