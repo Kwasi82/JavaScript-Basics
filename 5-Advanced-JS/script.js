94a90dc3662d73c1c5f353138af19fe554169fba
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-08 21:32:17 +0100

    Add a space in 'formal' if statement consol.log statement.

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -257,7 +257,7 @@ var kwasi = {
     job: 'Web Developer',
     presentation: function(style, timeOfDay) {
         if (style === 'formal') {
-            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' years old.');
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
         } else if (style === 'friendly') {
             console.log('Hey! What\'s up I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
         }
