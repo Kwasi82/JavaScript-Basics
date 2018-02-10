46053737720733874809e280ce1a47de3936d082
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-10 22:35:26 +0100

    Add array for questions.

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -338,3 +338,5 @@ var Question = function (question, answer, correct) {
 var questionOne = new Question ('Which avenger is from Wakanda?', ['0: Captain America','1: Winter Soldier','2: Black Panther'],2);
 var questionTwo = new Question ('What position did formal Chelsea Star Didier Drogba play?', ['0: Striker','1: Midfielder','2: Goalkeeper'],0);
 var questionThree = new Question ('Who is the current quarterback for the Dalles Cowboys?', ['Dak Prescott','Tony Romo','Roger Staubach'],0);
+
+var setOfQuestions = [questionOne,questionTwo,questionThree];
